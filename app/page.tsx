76ee21a375bc4e24dc35f835a78dcdc2f9d5bfba
himangshu <himangshu73@gmail.com>
2025-03-07 19:21:30 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,98 @@
+"use client";
+
+import axios from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+type Movie = {
+  title: string;
+  year: string;
+  imdb_id: string;
+  image_url?: string;
+};
+
+const Home = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function getLatestMovies() {
+      console.log("Fetching latest movies....");
+      try {
+        const response = await axios.get(
+          "https://movies-tv-shows-database.p.rapidapi.com/",
+          {
+            headers: {
+              "x-rapidapi-key":
+                "0f1646953emshf8d866fe8a4c87cp1e3482jsn1446e4418c6c",
+              "x-rapidapi-host": "movies-tv-shows-database.p.rapidapi.com",
+              Type: "get-recently-added-movies",
+            },
+            params: {
+              page: "100",
+            },
+          }
+        );
+        console.log(response.data);
+        const totalPages = Math.ceil(response.data.Total_results / 20);
+        console.log(totalPages);
+        const moviesList = response.data.movie_results || [];
+
+        const moviesWithImages = await Promise.all(
+          moviesList.map(async (movie: Movie) => {
+            const imageResponse = await axios.get(
+              "https://movies-tv-shows-database.p.rapidapi.com/",
+              {
+                headers: {
+                  "x-rapidapi-key":
+                    "0f1646953emshf8d866fe8a4c87cp1e3482jsn1446e4418c6c",
+                  "x-rapidapi-host": "movies-tv-shows-database.p.rapidapi.com",
+                  Type: "get-movies-images-by-imdb",
+                },
+                params: {
+                  movieid: movie.imdb_id,
+                },
+              }
+            );
+            console.log(imageResponse.data);
+            return {
+              ...movie,
+              image_url: imageResponse.data.poster || "",
+            };
+          })
+        );
+        setMovies(moviesWithImages);
+        console.log(moviesWithImages);
+      } catch (error) {
+        console.log("Error fetching latest movies.", error);
+      }
+    }
+    getLatestMovies();
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <h1>Movies List</h1>
+      <div>
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.imdb_id}>
+              <Image
+                src={movie.image_url || "/movie.jpg"}
+                alt={movie.title}
+                width={250}
+                height={350}
+              />
+              <div>
+                <h2>{movie.title}</h2>
+                <p>Year:{movie.year}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h2>No movies available.</h2>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
